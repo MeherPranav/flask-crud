2cd8d1fc5d350b5f4d781b19ec443288335328fc
Pranav Meher <pranav_meher@epam.com>
2024-08-01 21:09:30 +0530

    req

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+import os
 class Config:
-    SECRET_KEY = 'flask_app'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'password'
-    MYSQL_DB = 'flask'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MYSQL_HOST=os.getenv('DBEndpoint')
+    MYSQL_USER = os.getenv('DBUser')
+    MYSQL_PASSWORD = os.getenv('DBPassword')
+    MYSQL_DB = os.getenv('DBName')
+       
+    # SECRET_KEY='flask_app'
+    # MYSQL_HOST = 'localhost'
+    # MYSQL_USER = 'root'
+    # MYSQL_PASSWORD = 'password'
+    # MYSQL_DB = 'flask'
